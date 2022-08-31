a6f30ae5379196545da864651615fff014aa03df
Rodrigorov3r <rodrigoa.acevedo@gmail.com>
2022-08-30 22:22:31 -0300

    rutas ok

---
+++ routes/routes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import {
+  createBlog,
+  deleteBlog,
+  getAllBlogs,
+  getBlog,
+  updateBlog,
+} from '../controllers/BlogController';
+
+const rutas = express.Router();
+
+rutas.get('/', getAllBlogs);
+rutas.get('/:id', getBlog);
+rutas.post('/', createBlog);
+rutas.put('/:id', updateBlog);
+rutas.delete('/id', deleteBlog);
+
+export default rutas;
